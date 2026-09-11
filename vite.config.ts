caee1b9c1c3a26dfb399f1a4b6963824f4a118c0
Hype <michael.tillmanns@icloud.com>
2026-09-11 11:52:58 +0200

    feat: configure path aliases in Vite and TypeScript settings

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 import { defineConfig } from 'vitest/config';
 import tailwindcss from '@tailwindcss/vite';
+import * as path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +14,11 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'lcov'],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
