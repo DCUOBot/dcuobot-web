a8b676048cddbd694f4fdb566d25a21dfe73a60e
Hype <michael.tillmanns@icloud.com>
2026-09-11 10:19:47 +0200

    chore: update dev dependencies to include testing libraries and related tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+    },
+  },
 });
